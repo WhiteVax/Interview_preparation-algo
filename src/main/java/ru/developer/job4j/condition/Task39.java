7c82d2e3d7bbf19e86f95c9696d309fbf91316e8
white vax <whitevax@gmail.com>
2024-03-19 23:51:24 +0200

    Added exercises job4j.

---
+++ src/main/java/ru/developer/job4j/condition/Task39.java
@@ -0,0 +1,22 @@
+package ru.developer.job4j.condition;
+
+/**
+ * Написать код, который проверяет, является ли входящее число симметричным.
+ * Если является - вывести "Да", если нет - "Нет".
+ */
+public class Task39 {
+    public static void isSymmetric(int number) {
+        int first, third;
+        first = number / 100;
+        third = number % 10;
+        if (first == third) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(327 % 10);
+    }
+}
