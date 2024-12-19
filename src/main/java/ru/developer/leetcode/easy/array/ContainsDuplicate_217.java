33d4456d2ba72de4face2fecddd22f350bfb418c
white vax <whitevax@gmail.com>
2024-12-19 22:19:43 +0200

    Additing an easy solution 217 (Contains duplicate) LeetCode.

---
+++ src/main/java/ru/developer/leetcode/easy/array/ContainsDuplicate_217.java
@@ -0,0 +1,35 @@
+package ru.developer.leetcode.easy.array;
+import java.util.HashSet;
+
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ *
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false
+ * if every element is distinct.
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * Explanation:
+ * The element 1 occurs at the indices 0 and 3.
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Explanation:
+ * All elements are distinct.
+ *
+ * Example 3:
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ */
+public class ContainsDuplicate_217 {
+    public boolean containsDuplicate(int[] nums) {
+            var set = new HashSet<Integer>();
+            for(var num : nums) {
+                if(!set.add(num)) {
+                    return false;
+                }
+            }
+            return true;
+    }
+}
